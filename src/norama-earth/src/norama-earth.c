a643161dc6f00914847df3eecb20c557ee5ee284
niam-foxel <nils.hamel@foxel.ch>
2015-01-20 08:23:57 +0100

    Adding initial source of norama-earth

---
+++ src/norama-earth/src/norama-earth.c
@@ -0,0 +1,134 @@
+/*
+ * norama suite - Panorama tools suite
+ *
+ * Copyright (c) 2013-2014 FOXEL SA - http://foxel.ch
+ * Please read <http://foxel.ch/license> for more information.
+ *
+ *
+ * Author(s):
+ *
+ *      Nils Hamel <n.hamel@foxel.ch>
+ *
+ *
+ * This file is part of the FOXEL project <http://foxel.ch>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ *      You are required to attribute the work as explained in the "Usage and
+ *      Attribution" section of <http://foxel.ch/license>.
+ */
+
+
+/* 
+    Source - Includes
+ */
+
+    # include "norama-earth.h"
+
+/*
+    Source - Software main function
+ */
+
+    int main ( int argc, char ** argv ) {
+
+        /* Parallel processing variables */
+        int nrThread = 1;
+
+        /* Interpolation descriptor variables */
+        char nrMethod[256] = { 0 };
+
+        /* Image path variables */
+        char nriPath[256] = { 0 };
+        char nroPath[256] = { 0 };
+
+        /* Image allocation variables */
+        IplImage * nriImage = NULL;
+        IplImage * nroImage = NULL;
+
+        /* Search in parameters */
+        lc_stdp( lc_stda( argc, argv, "--input"        , "-i" ), argv,   nriPath , LC_STRING );
+        lc_stdp( lc_stda( argc, argv, "--output"       , "-o" ), argv,   nroPath , LC_STRING );
+        lc_stdp( lc_stda( argc, argv, "--interpolation", "-n" ), argv,   nrMethod, LC_STRING );
+        lc_stdp( lc_stda( argc, argv, "--threads"      , "-t" ), argv, & nrThread, LC_INT    );
+
+        /* Software swicth */
+        if ( lc_stda( argc, argv, "--help", "-h" ) || ( argc <= 1 ) ) {
+
+            /* Display usage */
+            fprintf( stdout, NR_HELP );
+
+        } else {
+
+            /* Import input image */
+            nriImage = cvLoadImage( nriPath, CV_LOAD_IMAGE_UNCHANGED );
+
+            /*  Verify input image reading */
+            if ( nriImage != NULL ) {
+
+                /* Create image allocation */
+                nroImage = cvCreateImage( cvSize( nriImage->width, nriImage->height ), IPL_DEPTH_8U , nriImage->nChannels );
+
+                /* Verify allocation creation */
+                if ( nroImage != NULL ) {
+
+                    /* Apply equirectangular transform */
+                    /*lg_transform_rotatep( 
+
+                        ( inter_C8_t * ) nriImage->imageData,
+                        ( inter_C8_t * ) nroImage->imageData,
+                        nriImage->width,
+                        nriImage->height,
+                        nriImage->nChannels,
+                        nrAzim * ( LG_PI / 180.0 ),
+                        nrElev * ( LG_PI / 180.0 ),
+                        nrRoll * ( LG_PI / 180.0 ),
+                        lc_method( nrMethod ),
+                        nrThread
+
+                    ); */
+
+                    /* Export output image */
+                    if ( cvSaveImage( nroPath, nroImage, NULL ) == 0 ) {
+
+                        /* Display message */
+                        fprintf( stdout, "Error : Unable to write output image\n" );
+
+                    }
+
+                    /* Release image memory */
+                    cvReleaseImage( & nroImage );
+
+                /* Display message */
+                } else { fprintf( stdout, "Error : Unable to create output image\n" ); }
+
+                /* Release image memory */
+                cvReleaseImage( & nriImage );
+
+            /* Display message */
+            } else { fprintf( stdout, "Error : Unable to read input image\n" ); }
+
+        }
+
+        /* Return to system */
+        return( EXIT_SUCCESS );
+
+    }
+
---
+++ src/norama-earth/src/norama-earth.h
@@ -0,0 +1,162 @@
+/*
+ * norama suite - Panorama tools suite
+ *
+ * Copyright (c) 2013-2014 FOXEL SA - http://foxel.ch
+ * Please read <http://foxel.ch/license> for more information.
+ *
+ *
+ * Author(s):
+ *
+ *      Nils Hamel <n.hamel@foxel.ch>
+ *
+ *
+ * This file is part of the FOXEL project <http://foxel.ch>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ *      You are required to attribute the work as explained in the "Usage and
+ *      Attribution" section of <http://foxel.ch/license>.
+ */
+
+    /*! \file   norama-earth.h
+     *  \author Nils Hamel <n.hamel@foxel.ch>
+     *   
+     *  Software main header
+     */
+
+    /*! \mainpage norama-rotate
+     *
+     *  \section norama-suite
+     *  \section _ Panorama tools suite
+     *
+     *  This software suite offers tools for numerical panoramas manipulation.
+     *  Mostly designed for equirectangular mappings, its role is to provide an
+     *  interface to libgnomonic and its algorithms. It then offers an interface
+     *  to projection and transformation features implemented in the gnomonic
+     *  library. Il also comes with a panorama viewer to complete the suite.
+     *
+     *  \section Documentation
+     *
+     *  A detailed documentation can be generated through doxygen. A more general
+     *  documentation can be consulted at https://github.com/FoxelSA/norama-suite/wiki
+     *
+     *  \section Copyright
+     * 
+     *  Copyright (c) 2013-2014 FOXEL SA - http://foxel.ch \n
+     *  This program is part of the FOXEL project <http://foxel.ch>.
+     *  
+     *  Please read the COPYRIGHT.md file for more information.
+     *
+     *  \section License
+     *
+     *  This program is licensed under the terms of the GNU Affero General Public
+     *  License v3 (GNU AGPL), with two additional terms. The content is licensed
+     *  under the terms of the Creative Commons Attribution-ShareAlike 4.0
+     *  International (CC BY-SA) license.
+     *
+     *  You must read <http://foxel.ch/license> for more information about our
+     *  Licensing terms and our Usage and Attribution guidelines.
+     */
+
+/* 
+    Header - Include guard
+ */
+
+    # ifndef __NR_EARTH__
+    # define __NR_EARTH__
+
+/* 
+    Header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    extern "C" {
+    # endif
+
+/* 
+    Header - Includes
+ */
+
+    # include <stdio.h>
+    # include <stdlib.h>
+    # include <opencv/cv.h>
+    # include <opencv/highgui.h>
+    # include <gnomonic-all.h>
+    # include <common-all.h>
+
+/* 
+    Header - Preprocessor definitions
+ */
+
+    /* Standard help */
+    # define NR_HELP "Usage summary :\n\n"                 \
+    "\tnorama-earth [Arguments] [Parameters] ...\n\n"      \
+    "Short arguments and parameters summary :\n\n"         \
+    "\t-i\tInput equirectangular mapping image\n"          \
+    "\t-o\tOutput equirectangular mapping image\n"         \
+    "\t-t\tNumber of threads\n"                            \
+    "\t-n\tInterpolation method\n\n"                       \
+    "norama-earth - norama-suite\n"                        \
+    "Copyright (c) 2013-2014 FOXEL SA - http://foxel.ch\n"
+
+/* 
+    Header - Preprocessor macros
+ */
+
+/* 
+    Header - Typedefs
+ */
+
+/* 
+    Header - Structures
+ */
+
+/* 
+    Header - Function prototypes
+ */
+
+    /*! \brief Software main function
+     *  
+     *  The main function frame follows : parameters are initialized and read.
+     *  The input image is loaded and the output image allocation is created.
+     *  The transformation is applied and the result is exported.
+     *  
+     *  \param argc Standard main parameter
+     *  \param argv Standard main parameter
+     */
+
+    int main ( int argc, char ** argv );
+
+/* 
+    Header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    }
+    # endif
+
+/*
+    Header - Include guard
+ */
+
+    # endif
+
+
